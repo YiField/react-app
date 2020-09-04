3ae9c9c895c99941cb5b6a24e8b99a9693b4ede4
sybil.chen <sybil.chen@huolala.cn>
2020-09-04 16:48:30 +0800

    init

---
+++ src/Clock.js
@@ -0,0 +1,48 @@
+import React from 'react'
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      counter: 123,
+      posts: [],
+      comments: []
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>lalala</h1>
+        <h2>{this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
+  componentDidMount() {
+    this.timer = setInterval(()=>{this.tick()},1000)
+    // fetchPosts().then(res => {
+    //   this.setState({
+    //     posts:res.posts
+    //   })
+    // })
+  }
+
+  componentWillUnmount() {
+    if(this.timer)clearInterval(this.timer)
+  }
+  tick() {
+    this.setState({
+      date:new Date()
+    })
+    this.setState((state, props)=>({
+      counter:state.counter + props.increment
+    }))
+    this.setState(function (state,props){
+      return {
+        counter:state.counter+props.increment
+      }
+    })
+  }
+
+}
+
+export default Clock
---
+++ src/ShoppingList.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>shopping-list for {this.props.name}</h1>
+        <ul>
+          <li>ins</li>
+          <li>whatsapp</li>
+          <li>oculus</li>
+        </ul>
+      </div>
+    )
+  }
+}
